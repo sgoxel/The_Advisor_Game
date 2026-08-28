8c207dd5b8ba002735df343cd765bc75a14778bf
sgoxel <163722426+sgoxel@users.noreply.github.com>
2026-08-28 22:44:40 +0300

    Load adjacent-region navigation module (#113)

--- js/utils.js
+++ js/utils.js
@@ -51,3 +51,4 @@ window.Game.Utils = {
 window.Game.Utils.loadScriptOnce("js/npc_world.js", "r02NpcWorldModule");
 window.Game.Utils.loadScriptOnce("js/world_composition.js", "r02WorldCompositionModule");
 window.Game.Utils.loadScriptOnce("js/game_time.js", "r02GameTimeModule");
+window.Game.Utils.loadScriptOnce("js/region_navigation.js", "r02RegionNavigationModule");
