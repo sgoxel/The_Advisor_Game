06657f2f50745129ec44e2ec6d20d7a2ad045136
sgoxel <163722426+sgoxel@users.noreply.github.com>
2026-08-28 23:02:04 +0300

    Load world hierarchy module (#134)

--- js/utils.js
+++ js/utils.js
@@ -55,3 +55,4 @@ window.Game.Utils.loadScriptOnce("js/region_navigation.js", "r02RegionNavigation
 window.Game.Utils.loadScriptOnce("js/region_time_progression.js", "r02RegionTimeProgressionModule");
 window.Game.Utils.loadScriptOnce("js/npc_life.js", "r02NpcLifeModule");
 window.Game.Utils.loadScriptOnce("js/ecology.js", "r02EcologyModule");
+window.Game.Utils.loadScriptOnce("js/world_hierarchy.js", "r02WorldHierarchyModule");
