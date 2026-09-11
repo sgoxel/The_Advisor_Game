ebae241cb38fead827f5c5200531d21cc48e58d3
sgoxel <163722426+sgoxel@users.noreply.github.com>
2026-09-11 21:16:29 +0300

    Load WP-002 I03 innkeeper adjacency gate (#432)

--- js/utils.js
+++ js/utils.js
@@ -124,6 +124,7 @@ window.Game.Utils.loadScriptOnce("js/protagonist_routine.js", "r04ProtagonistRou
 window.Game.Utils.loadScriptOnce("js/npc_residency.js", "r04NpcResidencyModule");
 window.Game.Utils.loadScriptOnce("js/npc_workplaces.js", "r04NpcWorkplacesModule");
 window.Game.Utils.loadScriptOnce("js/innkeeper_identity_lookup.js", "wp002InnkeeperIdentityLookupModule");
+window.Game.Utils.loadScriptOnce("js/innkeeper_adjacency_gate.js", "wp002InnkeeperAdjacencyGateModule");
 window.Game.Utils.loadScriptOnce("js/outdoor_worksites.js", "r04OutdoorWorksitesModule");
 window.Game.Utils.loadScriptOnce("js/guard_duty_anchors.js", "r04GuardDutyAnchorsModule");
 window.Game.Utils.loadScriptOnce("js/terrain_routing.js", "r04TerrainRoutingModule");
