77bbad7bef16b18149d739fe85d867b610783d97
sgoxel <163722426+sgoxel@users.noreply.github.com>
2026-09-21 15:54:37 +0300

    WP-006 update build marker

--- scripts/data/config.js
+++ scripts/data/config.js
@@ -1,5 +1,5 @@
 window.GameConfig=Object.freeze({
-  build:"WP-005",
+  build:"WP-006",
   defaultSeed:"The_Advisor_Game_20260924",
   campaignStorageKey:"theAdvisorGame.wp001.campaign.v2",
   settingsStorageKey:"theAdvisorGame.wp001.settings.v2",
