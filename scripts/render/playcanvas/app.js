66bcf0b7687aa6f4f9092514d778772db862fa30
sgoxel <163722426+sgoxel@users.noreply.github.com>
2026-09-24 23:32:47 +0300

    Expose diagonal road continuity bridge metrics

--- scripts/render/playcanvas/app.js
+++ scripts/render/playcanvas/app.js
@@ -853,7 +853,7 @@ function create({backendPreference="webgl2",maxPixelRatio=null,renderScale=null}
     let roadProfileRoadVertexCount=0,roadProfilePathVertexCount=0,roadProfileSquareVertexCount=0;
     let minRoadProfileDelta=Infinity,maxRoadProfileDelta=-Infinity,minRoadCoreHeightDelta=Infinity,maxRoadCoreHeightDelta=-Infinity;
     let routeSurfaceCellCount=0,routeMainRoadCellCount=0,routeLocalPathCellCount=0,routeSquareCellCount=0,routeConnectorCellCount=0;
-    let routeEdgeStripCount=0,routeSurfaceTriangleCount=0,routeNetworkConnectedRouteCount=0,routeNetworkTotalRouteCount=0;
+    let routeEdgeStripCount=0,routeDiagonalBridgeCount=0,routeSurfaceTriangleCount=0,routeNetworkConnectedRouteCount=0,routeNetworkTotalRouteCount=0;
     let routeSurfaceRouteSafe=true,routeNetworkRouteSafetyPass=true,routeSurfaceRendererOnly=true,routeNetworkDeterministic=true;
     const routeSurfaceSamples=[];
     const heightfieldResources=new Map();
@@ -929,6 +929,7 @@ function create({backendPreference="webgl2",maxPixelRatio=null,renderScale=null}
       routeSquareCellCount+=Number(resource.routeSquareCellCount||0);
       routeConnectorCellCount+=Number(resource.routeConnectorCellCount||0);
       routeEdgeStripCount+=Number(resource.routeEdgeStripCount||0);
+      routeDiagonalBridgeCount+=Number(resource.routeDiagonalBridgeCount||0);
       routeSurfaceTriangleCount+=Number(resource.routeSurfaceTriangleCount||0);
       routeNetworkConnectedRouteCount=Math.max(routeNetworkConnectedRouteCount,Number(resource.routeNetworkConnectedRouteCount||0));
       routeNetworkTotalRouteCount=Math.max(routeNetworkTotalRouteCount,Number(resource.routeNetworkTotalRouteCount||0));
@@ -1050,7 +1051,7 @@ function create({backendPreference="webgl2",maxPixelRatio=null,renderScale=null}
       bridgeClearanceWorldUnits:Number(generatorStats.bridgeClearanceWorldUnits||0),
       roadProfileGroundingShared:generatorStats.roadProfileGroundingShared===true,
       routeSurfaceCellCount,routeMainRoadCellCount,routeLocalPathCellCount,routeSquareCellCount,routeConnectorCellCount,
-      routeEdgeStripCount,routeSurfaceTriangleCount,
+      routeEdgeStripCount,routeDiagonalBridgeCount,routeSurfaceTriangleCount,
       routeSurfaceMaterialCount,
       routeSurfaceMaterialNames:generatorStats.routeSurfaceMaterialNames||Object.freeze([]),
       routeSurfaceMaterialRebinds:Number(generatorStats.routeSurfaceMaterialRebinds||0),
